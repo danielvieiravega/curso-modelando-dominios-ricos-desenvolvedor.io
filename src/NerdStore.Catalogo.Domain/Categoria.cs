4353fee47c99b526571f58306b79208b8bc88f11
Daniel Vieira Vega <vega@umbler.com>
2020-05-27 19:56:47 -0300

    finalizada aula Lançando ventos de domínio

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,34 @@
+﻿using NerdStore.Core.DomainObjects;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        protected Categoria() { }
+        
+        //EF relation
+        public ICollection<Produto> Produtos { get; private set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+
+            Validar();
+        }
+
+        public override string ToString() => $"{Nome} - {Codigo}";
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo não pode ser 0");
+        }
+    }
+}
