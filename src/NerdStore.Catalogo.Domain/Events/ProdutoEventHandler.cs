c7d85f52cfcd1a8f1bdc573e1ad825226618c79b
Daniel Vieira Vega <vega@umbler.com>
2020-06-02 20:15:09 -0300

    finalizada a aula 'Lançando eventos de domínio'

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+    }
+}
